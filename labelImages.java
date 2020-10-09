c66ee8a8b86ea516079e52eef0684571a5cce98a
erichVK5 <a1039181@gmail.com>
2020-10-09 23:32:48 +1030

    images now labelled with LITFL logo and CC-BY-NC licence

--- labelImages.java
+++ labelImages.java
@@ -12,7 +12,14 @@ public class labelImages {
     Scanner imagesAndLabels = new Scanner(rawData);
     int counter = 0;
     Boolean verbose = true;
+    Boolean addFilenameLabel = false;
     String previousImage = "";
+    String licencedLogo = "cc-by-nc-428x64.png";
+    int logoHeight = 64;
+    int logoWidth = 428;
+    int fontHeight = 35;
+    int textHeight = fontHeight * 4;
+    BufferedImage logo = ImageIO.read(new File(licencedLogo));
     while (imagesAndLabels.hasNextLine()) {
 	String[] data = imagesAndLabels.nextLine().split("\"");
 	String imageFile = data[0];
@@ -29,22 +36,37 @@ public class labelImages {
 		BufferedImage image = ImageIO.read(currentImage);
 		int origHeight = image.getHeight();
 		int origWidth = image.getWidth();
-		int textHeight = 35*4;
-		BufferedImage newImage = new BufferedImage(origWidth, origHeight + textHeight, BufferedImage.TYPE_INT_RGB);
+		int imgWidth = origWidth;
+		if (origWidth < 900) {
+			imgWidth = 900;
+		}
+		int imgOffset = (imgWidth - origWidth)/2;
+		BufferedImage newImage = new BufferedImage(imgWidth, origHeight + textHeight, BufferedImage.TYPE_INT_RGB);
 		for (int i = 0; i < origWidth; i++) {
 			for (int j = 0; j <  origHeight; j++) {
-				newImage.setRGB(i,j,image.getRGB(i,j));
+				newImage.setRGB(i + imgOffset,j,image.getRGB(i,j));
+			}
+		}
+		for (int i = 0; i < logoWidth; i++) {
+			for (int j = 0; j <  logoHeight; j++) {
+				newImage.setRGB(imgWidth - logoWidth + i, origHeight + textHeight - logoHeight + j, logo.getRGB(i,j));
 			}
 		}
 		Graphics g = newImage.getGraphics();
 		g.setFont(g.getFont().deriveFont(30f));
-		int currentTextYCoord = origHeight + textHeight - 35;
+		int currentTextYCoord = origHeight + textHeight - (fontHeight * 5)/2;
 		g.drawString(imageLabel, 10, currentTextYCoord);
 		String newFile = currentImage.getName();
-		currentTextYCoord -= 35;
-		g.drawString(newFile, 10, currentTextYCoord);
-		currentTextYCoord -= 35;
-		g.drawString("Image downloaded from litfl.com", 10, currentTextYCoord);
+		if (newFile.endsWith(".jpg") || newFile.endsWith(".JPG")
+ 			|| newFile.endsWith(".gif") || newFile.endsWith(".GIF")) {
+				newFile = newFile.substring(0,newFile.length()-4);
+		} else if (newFile.endsWith(".JPEG") || newFile.endsWith(".jpeg")) {
+				newFile = newFile.substring(0,newFile.length()-5);
+		}
+		if (addFilenameLabel) {
+			currentTextYCoord += fontHeight * 2;
+			g.drawString(newFile, 10, currentTextYCoord);
+		}
 		g.dispose();
 		ImageIO.write(newImage, "png", new File("modified/" + newFile + ".png"));
 		counter++;
